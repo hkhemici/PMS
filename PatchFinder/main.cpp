c74cf2ecdd31fe7521c3416039a2e4ab737b36c0
hkhemici <hkhemici@gmail.com>
2016-08-08 16:46:24 -0400

    HK: Added main loop to search multiple ports on a switch.

--- PatchFinder/main.cpp
+++ PatchFinder/main.cpp
@@ -25,14 +25,24 @@ void getSwitchPort(int& port)
 	cout << endl;
 }
 
+bool askTryAgain()
+{
+	cout << "\nDo you want to try another port on the same switch? (y/n): ";
+	char answer;
+	cin >> answer;
+	cout << "\n\n";
+	return answer == 'y' ? true : false;
+}
+
 int main()
 {
+
 	// Initiate variables
-	bool portUsable = true;
+	bool tryAgain = true;
 	cout << "\n////////////////////////////////////////////////////" <<
 		"\n//                                                //" <<
 		"\n//                 PATCH KING 3000                //" <<
-		"\n//                     v 1.0.0                    //" <<
+		"\n//                     v 1.0.1                    //" <<
 		"\n//                                                //" <<
 		"\n////////////////////////////////////////////////////" <<
 		"\n\n* Documentation on https://github.com/hkhemici/PMS *\n";
@@ -41,64 +51,65 @@ int main()
 	string name;
 	getSwitchName(name);
 
-	// Get switch number.
-	int number;
-	getSwitchNumber(number);
-
-	// Get switch port.
-	int port;
-	getSwitchPort(port);
-
-	// Create switch object.
-	Switch PmsSwitch(name, number, port);
-
-	// _______________________________________________________
-	// TEMPORARY, WILL BECOME \\MONITOR02...
-	vector<string> localNames;
-	for (int i = 0; i < 90; i++)
+	while (tryAgain)
 	{
-		//localNames.push_back("C:\\Users\\hkhemici\\Documents\\RM-SW-52\\" + PmsSwitch.getLogs().getList()[i]);
-		localNames.push_back("\\\\MONITOR02-RM\\Open_Switch_Ports$\\" + PmsSwitch.getName() + "\\" + PmsSwitch.getLogs().getList()[i]);
-	}
-	// _______________________________________________________
+		// Initiate portUsable variable for each loop.
+		bool portUsable = true;
 
+		// Get switch number.
+		int number;
+		getSwitchNumber(number);
 
-	// test Reader class with given switch log file.
-	Reader reader;
+		// Get switch port.
+		int port;
+		getSwitchPort(port);
 
+		// Create switch object.
+		Switch PmsSwitch(name, number, port);
 
-	for (int i = 0; i < 90; i++)
-	{
-		try
+		// Build localNames.
+		vector<string> localNames;
+		for (int i = 0; i < 90; i++)
 		{
-			if (reader.checkPortInFile(localNames[i], number, port))
-			{
-				cout << "Port " << port << " is available on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
-			}
-			else
-			{
-				cout << "Port " << port << " is -- NOT AVAILABLE -- on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
-				portUsable = false;
-			}
+			//localNames.push_back("C:\\Users\\hkhemici\\Documents\\RM-SW-52\\" + PmsSwitch.getLogs().getList()[i]);
+			localNames.push_back("\\\\MONITOR02-RM\\Open_Switch_Ports$\\" + PmsSwitch.getName() + "\\" + PmsSwitch.getLogs().getList()[i]);
 		}
-		catch (const std::exception& e)
+
+		// Read files.
+		Reader reader;
+
+
+		for (int i = 0; i < 90; i++)
 		{
-			cout << e.what() << " on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
+			try
+			{
+				if (reader.checkPortInFile(localNames[i], number, port))
+				{
+					cout << "Port " << port << " is available on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
+				}
+				else
+				{
+					cout << "Port " << port << " is -- NOT AVAILABLE -- on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
+					portUsable = false;
+				}
+			}
+			catch (const std::exception& e)
+			{
+				cout << e.what() << " on " << PmsSwitch.getLogs().getDates().getYYYYMMDD()[i] << endl;
+			}
+
 		}
-		
-	}
 
-	// Final message.
-	if (portUsable)
-	{
-		cout << "\nThe port CAN be used to patch a user!\n" << endl;
-	}
-	else
-	{
-		cout << "\nThe port CANNOT be used to patch a user.\n" << endl;
-	}
+		// Final message.
+		if (portUsable)
+		{
+			cout << "\nThe port CAN be used to patch a user!\n" << endl;
+		}
+		else
+		{
+			cout << "\nThe port CANNOT be used to patch a user.\n" << endl;
+		}
 
-	// Prevent immediate closing.
-	system("pause");
-
+		tryAgain = askTryAgain();
+	}
 }
